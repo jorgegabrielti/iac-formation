3d2f700134a669a700b197524f0932c84aae6460
jorgegabrielti <jorgegabriel.ti@gmail.com>
2022-07-14 12:27:28 -0300

    [add] firt commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "terraform"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0cff7528ff583bf9a"
+  instance_type = "t2.micro"
+  key_name = "iac-alura"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
