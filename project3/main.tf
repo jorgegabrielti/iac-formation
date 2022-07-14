2a32cb8f2c747c8d80895e341bb5a85c02906e29
jorgegabrielti <jorgegabriel.ti@gmail.com>
2022-07-14 18:22:29 -0300

    [update] structure of directories

---
+++ project3/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "terraform"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-052efd3df9dad4825"
+  instance_type = "t2.micro"
+  key_name = "iac-alura"
+  tags = {
+    Name = "Terraform Ansible Python"
+  }
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name = DEV
+  public_key = file("development.pem.pub")
+}
