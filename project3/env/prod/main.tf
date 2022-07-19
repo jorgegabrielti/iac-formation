beecdc581d823e6e6771f912b0a3a9a140490b8a
Jorge Gabriel <jorgegabriel.it@gmail.com>
2022-07-18 21:46:59 -0300

    [update]

--- project3/env/prod/main.tf
+++ project3/env/prod/main.tf
@@ -1,7 +1,11 @@
 module "aws_dev" {
   source        = "../../infra"
-  instance_type = "t2.medium"
+  instance_type = "t2.micro"
   ami           = "ami-052efd3df9dad4825"
   region        = "us-east-1"
   ssh_key       = "production.pem"
+}
+
+output "ip" {
+  value = module.aws_dev.public_ip
 }
